4047a8d5d9e6ea132136861719d635fbdac056e1
zhangyang <zhangyang@ec.e-chemchina.com>
2020-05-10 20:06:41 +0800

    mp 开启AR模式

--- src/main/java/cn/myfreecloud/entity/User.java
+++ src/main/java/cn/myfreecloud/entity/User.java
@@ -4,7 +4,9 @@ import com.baomidou.mybatisplus.annotation.SqlCondition;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 
@@ -14,8 +16,11 @@ import java.time.LocalDateTime;
  * @description:
  */
 @Data
+@EqualsAndHashCode(callSuper = false)
 @TableName("user")
-public class User {
+public class User extends Model<User> {
+
+    private static final long serialVersionUID = -3466433117332133021L;
 
     /** MP默认回去查找字段为id的值作为主键,不是id的话需要标注 @TableId 这个注解**/
     @TableId
